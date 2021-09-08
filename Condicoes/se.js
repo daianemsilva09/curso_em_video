a821c1fc2382c5665e802122cf7ab8f2012c85e7
Daiane Silva <daianemsilva09@gmail.com>
2021-09-08 17:24:40 -0300

    até aula 11

---
+++ Condicoes/se.js
@@ -0,0 +1,17 @@
+var vel = 70
+console.log(`a velocidade é ${vel} Km/h`)
+if (vel > 60) { //condição simples
+    console.log(`Multa`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
+
+
+console.log(`Ex 2`)
+var pais = 'Brasil'
+console.log(`vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+}else{
+    console.log(`Você é Estrangeiro`)
+}
+
